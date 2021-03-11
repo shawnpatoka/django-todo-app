7275bfea20a8d5b551bd66a0e06ebfb5f95b1407
Shawn Patoka <accounts@shawnpatoka.com>
2021-03-11 10:27:47 -0600

    initial commit

---
+++ tasks/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.index, name="list"),
+    path('update-task/<str:pk>/', views.updateTask, name="update"),
+    path('delete-task/<str:pk>/', views.deleteTask, name="delete"),
+    path('tick-task/<str:pk>/', views.taskTicked, name="tick_task"),
+    path('untick-task/<str:pk>/', views.taskUnticked, name="untick_task"),
+
+]
