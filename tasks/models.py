35fcae24f2237d7ae1d3183abdb701f3347331d0
Shawn Patoka <accounts@shawnpatoka.com>
2021-03-15 16:21:20 -0500

    added initial categories

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+COLORS = [
+    ('gray', 'Gray'),
+    ('blue', 'Blue'),
+    ('purple', 'Purple'),
+    ('red', 'Red'),
+    ('green', 'Green'),
+]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200, choices=COLORS, default="gray")
 
     def __str__(self):
         return self.name
@@ -18,7 +27,8 @@ class Task(models.Model):
     extra_info      = models.CharField(max_length=300, blank=True)
     complete        = models.BooleanField(default=False)
     created         = models.DateTimeField(auto_now_add=True)
-    category        = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+    # category        = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+    category        = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.title
